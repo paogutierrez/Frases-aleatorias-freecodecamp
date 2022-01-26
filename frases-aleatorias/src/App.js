61bf0168c313751869ff7b25624685101bda556f
paogutierrez <paola.gutierrez.yaquibe@gmail.com>
2022-01-26 13:19:03 -0500

    frases aleatorias terminado

---
+++ frases-aleatorias/src/App.js
@@ -0,0 +1,57 @@
+import React, {useEffect, useState} from 'react'
+import './App.scss';
+import COLORS_ARRAY from "./ColorsArray"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+
+
+let quoteDBUrl = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
+
+function App() {
+  const [quote, setQuote] = useState("There are no traffic jams along the extra mile.");
+  const [author, setAuthor] = useState("Roger Staubach");
+  const [randomNumber, setRandomNumber] = useState(0);
+  const [quotesArray, setQuotesArray] = useState(null);
+  const [accentColor, setAccentColor] = useState('#282c34')
+
+  const fetchQuotes = async (url) => {
+    const response = await fetch (url)
+    const parsedJSON = await response.json()
+    setQuotesArray(parsedJSON.quotes)
+    console.log(parsedJSON)
+  }
+
+  useEffect(() => {
+    fetchQuotes(quoteDBUrl)
+  }, [quoteDBUrl])
+
+  const getRandomQuote = () => {
+    let randomInteger = Math.floor(quotesArray.length * Math.random())
+    setRandomNumber(randomInteger)
+    setAccentColor(COLORS_ARRAY[randomInteger])
+    setQuote(quotesArray[randomInteger].quote)
+    setAuthor(quotesArray[randomInteger].author)
+    
+  }
+
+  return (
+    <div className="App">
+      <header className="App-header" style={{backgroundColor:accentColor}}>
+        <div id="quote-box" style={{color:accentColor}}>
+      <h1></h1>
+        <p id ="text">
+        "{quote}"
+        </p>
+       <p id="author">- {author}</p> 
+       <div className="buttons">
+       <a id="tweet-quote" style={{backgroundColor:accentColor}} href='http://www.twitter.com/intent/tweet?text'><FontAwesomeIcon icon={faTwitter}/></a>
+       </div>
+       <button id="new-quote" style={{backgroundColor:accentColor}} onClick={()=> getRandomQuote ()}>Generate A Random Quote</button>
+       </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
